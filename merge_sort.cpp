b30855401a5673653d60f929c4c1b69106ca726c
Anjali Pathak <03anjalipathak@gmail.com>
2023-06-24 08:00:00 +0530

    Create merge_sort.cpp

---
+++ merge_sort.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> left_arr(n1), right_arr(n2);
+
+    for (int i = 0; i < n1; i++)
+        left_arr[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        right_arr[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (left_arr[i] <= right_arr[j]) {
+            arr[k] = left_arr[i];
+            i++;
+        }
+        else {
+            arr[k] = right_arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = left_arr[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = right_arr[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+void printArray(const vector<int>& arr) {
+    for (int num : arr)
+        cout << num << " ";
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {12, 11, 13, 5, 6, 7};
+
+    cout << "Original array: ";
+    printArray(arr);
+
+    mergeSort(arr, 0, arr.size() - 1);
+
+    cout << "Sorted array: ";
+    printArray(arr);
+
+    return 0;
+}
